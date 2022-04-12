fa7dc3b55e12ef4a6283afa7ec21d7f323f360ef
aliajeli <mr.ali.ajeli@hotmail.com>
2022-04-12 13:00:05 +0430

    broken our homepage into smaller components, finished styling our menu-item

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import DirectoryMenu from "../../components/DirectoryMenu/DirectoryMenu";
+import "./HomePage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  );
+};
+export default HomePage;
